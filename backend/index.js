79bb6d5a43cde914605c1a3ac485bf32b73c2059
katebry <katebryan31@gmail.com>
2022-01-23 07:47:46 -0800

    feat: added socketid and players array

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,30 @@ const server = require('http').createServer(app);
 const port = 3000
 const io = require('socket.io')(server);
 
+let players = [];
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('someone connected!')
+    console.log('A user connected: ' + socket.id);
+    players.push(socket.id);
+
+    if (players.length === 1) {
+        io.emit('isPlayer1');
+    };
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        players = players.filter(player => player !== socket.id);
     });
+
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     });
+
+    console.log(players)
 });
 
 server.listen(port, () => {
