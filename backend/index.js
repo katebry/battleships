ebf1311629258c09cfcc7be8555aa227b9a9cd20
katebry <katebryan31@gmail.com>
2022-01-23 19:23:34 -0800

    chore: fixed cors error on backend

--- backend/index.js
+++ backend/index.js
@@ -1,68 +1,44 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const port = 3000
-const io = require('socket.io')(server);
+const express = require("express")();
+const cors = require("cors");
+const httpServer = require("http").Server(express);
 
-const connectionLimit = 2;
-let players = [];
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"]
+    }
+});
 
-let position = {
+var position = {
     x: 200,
     y: 200
 };
 
+httpServer.listen(3000, () => {
+    console.log("Listening at :3000...");
+});
 
-io.on('connection', (socket) => {
-    console.log('A user connected: ' + socket.id);
-    players.push(socket.id);
-
-    if (io.engine.clientsCount > connectionLimit) {
-        socket.emit('err', { message: 'reach the limit of connections' })
-        socket.disconnect()
-        console.log('Disconnected...')
-        return
-    }
-
-    if (players.length === 1) {
-        io.emit('isPlayer1');
-    };
-
+io.on("connection", socket => {
+    socket.emit("position", position);
     socket.on("move", data => {
         switch(data) {
             case "left":
                 position.x -= 5;
-                Socketio.emit("position", position);
+                io.emit("position", position);
                 break;
             case "right":
                 position.x += 5;
-                Socketio.emit("position", position);
+                io.emit("position", position);
                 break;
             case "up":
                 position.y -= 5;
-                Socketio.emit("position", position);
+                io.emit("position", position);
                 break;
             case "down":
                 position.y += 5;
-                Socketio.emit("position", position);
+                io.emit("position", position);
                 break;
         }
     });
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        players = players.filter(player => player !== socket.id);
-    });
-
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-
-    console.log(players)
 });
-
-server.listen(port, () => {
-    console.log(`server listening on port ${port}`)
-});
-
-
 
--- client/src/BlockGame.vue
+++ client/src/BlockGame.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <canvas ref="game" width="640" height="480" style="border: 1px solid black;"></canvas>
+    <p>
+      <button v-on:click="move('right')">Right</button>
+      <button v-on:click="move('left')">Left</button>
+      <button v-on:click="move('up')">Up</button>
+      <button v-on:click="move('down')">Down</button>
+    </p>
   </div>
 </template>
 
@@ -18,14 +24,22 @@ export default {
       }
     }
   },
-  created() { },
+  created() {
+    this.socket = io("http://localhost:3000");
+  },
   mounted() {
     this.context = this.$refs.game.getContext("2d");
+    this.socket.on("position", data => {
+      this.position = data;
+      this.context.clearRect(0, 0, this.$refs.game.width, this.$refs.game.height);
+      this.context.fillStyle = "#FFFFFF";
+      this.context.fillRect(0, 0, this.$refs.game.width, this.$refs.game.width);
+      this.context.fillStyle = "#000000";
+      this.context.fillRect(this.position.x, this.position.y, 20, 20);
+    });
   },
   methods: {
-    created() {
-      this.socket = io("http://localhost:3000");
-    },
+    move(direction) { this.socket.emit("move", direction); },
   }
 }
 </script>
