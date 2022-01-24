57d95245ff23766a7045bdd54c59a9ec8dce4e5e
katebry <katebryan31@gmail.com>
2022-01-23 19:00:16 -0800

    feat: playing around with frontend components

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,50 @@ const server = require('http').createServer(app);
 const port = 3000
 const io = require('socket.io')(server);
 
+const connectionLimit = 2;
 let players = [];
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+let position = {
+    x: 200,
+    y: 200
+};
+
 
 io.on('connection', (socket) => {
     console.log('A user connected: ' + socket.id);
     players.push(socket.id);
 
+    if (io.engine.clientsCount > connectionLimit) {
+        socket.emit('err', { message: 'reach the limit of connections' })
+        socket.disconnect()
+        console.log('Disconnected...')
+        return
+    }
+
     if (players.length === 1) {
         io.emit('isPlayer1');
     };
+
+    socket.on("move", data => {
+        switch(data) {
+            case "left":
+                position.x -= 5;
+                Socketio.emit("position", position);
+                break;
+            case "right":
+                position.x += 5;
+                Socketio.emit("position", position);
+                break;
+            case "up":
+                position.y -= 5;
+                Socketio.emit("position", position);
+                break;
+            case "down":
+                position.y += 5;
+                Socketio.emit("position", position);
+                break;
+        }
+    });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <BlockGame/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlockGame from './BlockGame.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BlockGame
   }
 }
 </script>
